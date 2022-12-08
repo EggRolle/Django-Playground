9d85451b702b2eeee44d76445241a0755ef7f7ec
Nophil Mehboob <58795457+EggRolle@users.noreply.github.com>
2022-12-08 18:41:11 -0500

    Add files via upload

---
+++ storefront/ollApp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+# request to response
+
+#action
+
+def  old_sample(request):
+    
+    return render(request,'myLearningKit_Ex5.html')
